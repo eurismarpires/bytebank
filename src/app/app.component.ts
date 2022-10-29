2c6342f31adff5168fcea9de26efed8593e69357
eurismarpires <eurismarpires@gmail.com>
2022-10-29 14:00:32 -0300

    Propagação de evento e diretiva @Output.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bytebank';
+  destino: number = 0;
+  valor: number = 0;
+
+  transferir($event: any){
+    this.valor = $event.valor;
+    this.destino = $event.destino;
+  }
 }
+
--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-nova-transferencia',
@@ -7,11 +7,15 @@ import { Component } from '@angular/core';
 })
 export class NovaTranferenciaComponent {
 
+ @Output() aoTransferir = new EventEmitter<any>();
+
   valor: number = 0;
   destino: number = 999;
   transferir(): void {
     console.log('Socilitar nova transferência');
     console.log('valor:', this.valor);
     console.log('destino:', this.destino);
+    const valorEmitir = {valor: this.valor, destino: this.destino}
+    this.aoTransferir.emit(valorEmitir)
   }
 }
